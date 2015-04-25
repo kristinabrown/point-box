ae485076ae504815f47578d250dd53706869e707
Kristina Brown <kristina.frey.frey@gmail.com>
2015-04-25 11:42:13 -0600

    paperclip added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150423194855) do
+ActiveRecord::Schema.define(version: 20150425163910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,12 @@ ActiveRecord::Schema.define(version: 20150423194855) do
     t.string   "name"
     t.string   "description"
     t.integer  "cost"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "user_rewards", force: :cascade do |t|
